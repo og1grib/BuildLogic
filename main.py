7dee419c091c1a7da741135a41359bdb44c0229c
Grigorii Gerasimov <grigorii@MacBook-Pro-Grigorii.local>
2024-08-20 12:45:27 +0300

    local ssgs add

--- main.py
+++ main.py
@@ -19,17 +19,86 @@ def connect_db():
     conn.autocommit = True
     return conn
 
+def calculate_cpm(cur, df_operations):
+    operations = prepare_operations(df_operations) # Словарь из operations
+
+    critical_path, total_duration = cpm(operations)
+    print("Critical Path:", critical_path)
+    print("CPM Total Duration of the Project:", total_duration)
+    # plot_gantt_chart(operations)
+    insert_results_to_table(cur, operations)
+
+def calculate_rcpm(cur, df_operations, df_resources):
+    operations = prepare_operations(df_operations)
+
+    critical_path, total_duration = rcpm(operations, df_resources)
+    print("Critical Path:", critical_path)
+    print("RCPM Total Duration of the Project:", total_duration)
+
+    check_resource_conflicts(operations, df_resources) # Проверка конфликт ресурсов
+    check_precedence_relations(operations) # Проверка конфликт предшествоания
+    # plot_gantt_and_resource_chart(operations, df_resources)
+
+    insert_results_to_table(cur, operations)
+
+def calculate_ssgs(cur, df_operations, df_resources):
+    operations = prepare_operations(df_operations)
+
+    critical_path, total_duration = ssgs(operations, df_resources)
+    print("Critical Path:", critical_path)
+    print("SSGS Total Duration of the Project:", total_duration)
+
+    check_resource_conflicts(operations, df_resources)
+    check_precedence_relations(operations)
+    # plot_gantt_and_resource_chart(operations, df_resources)
+
+    insert_results_to_table(cur, operations)
+
+def calculate_rcpm_with_local_sgs(cur, df_operations, df_resources, selected_tasks, use_pr=False):
+    operations = prepare_operations(df_operations)
+
+    critical_path, total_duration = rcpm(operations, df_resources)
+    total_duration = local_ssgs(operations, df_resources, selected_tasks, use_pr=use_pr)
+
+    print("Critical Path:", critical_path)
+    print("RCPM with local SSGS Total Duration of the Project:", total_duration)
+
+    check_resource_conflicts(operations, df_resources)
+    check_precedence_relations(operations)
+    # plot_gantt_and_resource_chart(operations, df_resources)
+
+    insert_results_to_table(cur, operations)
+
 if __name__ == "__main__":
     conn = connect_db()
     cur = conn.cursor()
 
-    act = input("Выберите действие: create_tables, drop_table, drop_all_table, insert_csv, insert_manual, calculate_cpm, calculate_rcpm, calculate_ssgs, export_table_to_csv: ")
+    act = input("Выберите действие: create_tables, drop_table, drop_all_tables, insert_csv, insert_manual, calculate_cpm, calculate_rcpm, calculate_ssgs, calculate_rcpm_with_local_sgs, export_table_to_csv: ")
+    df_operations = pd.read_sql("SELECT * FROM operations", conn)
+    df_resources = pd.read_sql("SELECT * FROM resources", conn)
+    selected_tasks = [
+                            # "TASK1/_/1",
+                            # "TASK1/_/2",
+                            # "TASK1/_/3",
+                            "TASK2/_/4",
+                            "TASK1/_/5",
+                            "TASK2/_/6",
+                            "TASK3/_/7",
+                            "TASK3/_/8",
+                            "TASK3/_/9",
+                            "TASK4/_/10",
+                            "TASK4/_/11",
+                            "TASK4/_/12",
+                            # "TASK4/_/13",
+                            # "TASK4/_/14",
+                            # "TASK4/_/15"
+                        ]
 
     if act == "create_tables":
         create_tables(cur)
     elif act == "drop_table":
         drop_table(cur, "results")
-    elif act == "drop_all_table":
+    elif act == "drop_all_tables":
         drop_all_tables(cur)
     elif act == "insert_csv":
         insert_operations_from_csv(cur, 'data/operations.csv')
@@ -40,45 +109,17 @@ if __name__ == "__main__":
         insert_add_info(cur)
 
     elif act == "calculate_cpm":
-        df_operations = pd.read_sql("SELECT * FROM operations", conn)
-        operations = prepare_operations(df_operations) # Словарь из operations
-
-        critical_path, total_duration = cpm(operations)
-        print("Critical Path:", critical_path)
-        print("CPM Total Duration of the Project:", total_duration)
-        plot_gantt_chart(operations)
-        insert_results_to_table(cur, operations)
+        calculate_cpm(cur, df_operations)
 
     elif act == "calculate_rcpm":
-        df_operations = pd.read_sql("SELECT * FROM operations", conn)
-        df_resources = pd.read_sql("SELECT * FROM resources", conn)
-        operations = prepare_operations(df_operations)
-
-        critical_path, total_duration = rcpm(operations, df_resources)
-        print("Critical Path:", critical_path)
-        print("RCPM Total Duration of the Project:", total_duration)
-
-        check_resource_conflicts(operations, df_resources) # Проверка конфликт ресурсов
-        check_precedence_relations(operations) # Проверка конфликт предшествоания
-        plot_gantt_and_resource_chart(operations, df_resources)
-
-        insert_results_to_table(cur, operations)
+        calculate_rcpm(cur, df_operations, df_resources)
 
     elif act == "calculate_ssgs":
-        df_operations = pd.read_sql("SELECT * FROM operations", conn)
-        df_resources = pd.read_sql("SELECT * FROM resources", conn)
-        operations = prepare_operations(df_operations)
+        calculate_ssgs(cur, df_operations, df_resources)
 
-        critical_path, total_duration = ssgs(operations, df_resources)
-        print("Critical Path:", critical_path)
-        print("SSGS Total Duration of the Project:", total_duration)
-
-        check_resource_conflicts(operations, df_resources)
-        check_precedence_relations(operations)
-        plot_gantt_and_resource_chart(operations, df_resources)
-
-        insert_results_to_table(cur, operations)
-
+    elif act == "calculate_rcpm_with_local_sgs":
+        calculate_rcpm_with_local_sgs(cur, df_operations, df_resources, selected_tasks)
+        
     elif act == "export_results_to_csv":
         export_table_to_csv(conn, 'results', 'results_output.csv')
     
--- src/algorithms/ssgs.py
+++ src/algorithms/ssgs.py
@@ -14,7 +14,7 @@ def ssgs(operations, df_resources, use_pr=False):
         eligible_activities = [act for act in operations if act not in scheduled and all(pre in scheduled for pre in operations[act]['predecessors'])]
 
         if not eligible_activities:
-            print('Расписание нельзя составить.')
+            print('Расписание нельзя составить!!!')
             break
 
         # min-lft приоритет
@@ -55,3 +55,68 @@ def ssgs(operations, df_resources, use_pr=False):
 
     total_duration = max(op['early_finish'] for op in operations.values())
     return critical_path, total_duration
+
+
+def local_ssgs(operations, df_resources, selected_tasks, use_pr=True):
+
+    resources = {res['type']: res['quantity'] for _, res in df_resources.iterrows()}
+    max_time = sum(op['duration'] for op in operations.values())
+    resource_availability = {r: [resources[r]] * max_time for r in resources.keys()}   
+
+    for act, op in operations.items():
+        if act not in selected_tasks:
+            for r in op['resources']:
+                for time_slot in range(op['early_start'], op['early_finish']):
+                    resource_availability[r][time_slot] -= 1
+
+    start_times = {}
+    finish_times = {}
+    scheduled = []
+    
+    selected_operations = {task: operations[task] for task in selected_tasks}
+
+    while len(scheduled) < len(selected_operations):
+        eligible_activities = [act for act in selected_operations if act not in scheduled]
+
+        if not eligible_activities:
+            print('Расписание нельзя обновить!!!')
+            break
+
+        # min-lft приоритет
+        if use_pr:
+            pr = {act: selected_operations[act]['late_finish'] for act in eligible_activities}
+            current_act = min(pr, key=pr.get)
+        else:
+            current_act = eligible_activities[0]
+
+        earliest_start = max(finish_times.get(pre, 0) for pre in selected_operations[current_act]['predecessors']) if selected_operations[current_act]['predecessors'] else 0
+        duration = selected_operations[current_act]['duration']
+
+        eligible_times = []
+        for t in range(earliest_start, max_time):
+            if all(resource_availability[r][t:t + duration].count(resources[r]) >= duration for r in selected_operations[current_act]['resources']):
+                eligible_times.append(t)
+
+        if eligible_times:
+            start_time = min(eligible_times)
+            finish_time = start_time + duration
+            start_times[current_act] = start_time
+            finish_times[current_act] = finish_time
+            scheduled.append(current_act)
+
+            for r in selected_operations[current_act]['resources']:
+                for time_slot in range(start_time, finish_time):
+                    resource_availability[r][time_slot] -= 1
+        else:
+            print(f"Операция {current_act} не может быть поставлена в расписание.")
+            
+    # Обновление всех времен
+    for act, start_time in start_times.items():
+        delta = start_time - operations[act]['early_start']
+        operations[act]['early_start'] += delta
+        operations[act]['early_finish'] += delta
+        operations[act]['late_start'] += delta
+        operations[act]['late_finish'] += delta
+
+    total_duration = max(op['early_finish'] for op in operations.values())
+    return total_duration
